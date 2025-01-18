2e71bd8cafd417e2b604265f8e92b1a82b934c2f
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2025-01-19 00:46:22 +0200

    Validare in proces de lucru

--- Tratament.Web/Controllers/SendRequestController.cs
+++ Tratament.Web/Controllers/SendRequestController.cs
@@ -1,14 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Tratament.Web.ViewModels.SendRequest;
 
 namespace Tratament.Web.Controllers
 {
     public class SendRequestController : Controller
     {
 
-
+        [HttpGet]
         public IActionResult Send()
         {
-            return View();
+            SendRequestViewModel requestViewModel = new();
+
+            ViewBag.TicketTypes = GetTicketTypes();
+
+
+            return View(requestViewModel);
+        }
+
+
+        [HttpPost]
+        public IActionResult Send(SendRequestViewModel requestViewModel)
+        {
+
+            ViewBag.TicketTypes = GetTicketTypes();
+
+
+            return View(requestViewModel);
+        }
+
+
+        private List<SelectListItem> GetTicketTypes()
+        {
+            List<SelectListItem> pensionType = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Bilete pentru persoanele cu dizabilităţi", Value = "1" },
+                new SelectListItem { Text = "Bilete pentru veterani", Value = "2" },
+                new SelectListItem { Text = "Bilete pentru Cernobîl", Value = "3" },
+                new SelectListItem { Text = "Compensaţia bănească în schimbul biletului pentru Cernobîl", Value = "4" }
+            };
+
+            return pensionType;
         }
     }
 }
---
+++ Tratament.Web/ViewModels/SendRequest/SendRequestViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tratament.Web.ViewModels.SendRequest
+{
+    public class SendRequestViewModel
+    {
+        public SendRequestViewModel()
+        {
+            PenionType = new List<SelectListItem>();
+        }
+
+        [Required(ErrorMessage = @"Acest cîmp este obligator.")]
+        [StringLength(13, MinimumLength = 13, ErrorMessage = @"IDNP-ul trebuie să fie din 13 cifre.")]
+        public string Idnp { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = @"Acest cîmp este obligator.")]
+        public string TicketTypeId { get; set; }
+
+        public List<SelectListItem> PenionType { get; set; }
+    }
+}
