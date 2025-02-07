16c0138e66f383ae093ccfc9fdbf0c7eb874fcbb
mihai <m.tamazlicaru1959@gmail.com>
2025-02-07 15:27:36 +0200

    Eliminare Partial View, care nu vor fi folosite.

---
+++ Tratament.Model/Models/Enums/ServiceTypeEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tratament.Model.Models.Enums
+{
+    public enum ServiceTypeEnum
+    {
+        Disabilities = 1,
+        Veterans = 2,
+        Cernobil = 3,
+        PaymentCompensation = 4
+    }
+}
