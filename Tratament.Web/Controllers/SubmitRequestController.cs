ac614f1526ae02fb279e57e15d0ebad8603064a2
mihai <m.tamazlicaru1959@gmail.com>
2025-02-12 15:28:51 +0200

    A fost creat View-ul Error. A fost creat controleul SubmireRequest Controller

---
+++ Tratament.Web/Controllers/SubmitRequestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tratament.Web.Controllers
+{
+    public class SubmitRequestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tratament.Web/ViewModels/Error/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Tratament.Web.ViewModels.Error
+{
+    public class ErrorViewModel
+    {
+        public string ErrorNumber { get; set; }
+        public string ErrorText { get; set; }
+    }
+}
