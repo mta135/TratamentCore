647da50baa3039087360174fa866901d98fb2266
mihai <m.tamazlicaru1959@gmail.com>
2025-02-12 13:13:18 +0200

    Crearea modelului TicketServiceModel

---
+++ Tratament.Model/Models/EcerereTicketService/TicketServiceModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tratament.Model.Models.EcerereTicketService
+{
+    public class TicketServiceModel
+    {
+        public short? Vpres_rf { get; set; }
+
+        public string Vidnp { get; set; }
+
+        public string Vnume { get; set; }
+
+        public string Vprenume { get; set; }
+
+        public string Vcuatm { get; set; }
+
+        public string Vadresa { get; set; }
+
+        public string Vtelefon { get; set; }
+
+        public string Vemail { get; set; }
+
+        public DateTime? VnascutD { get; set; }
+
+        public string Vsex { get; set; }
+    }
+}
